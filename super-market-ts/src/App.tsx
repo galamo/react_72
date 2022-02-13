f1355b088310ab283da155494c1b1128123d0bb1
galamo <galamouyal88@gmail.com>
2022-02-13 21:45:40 +0200

    hw

--- super-market-ts/src/App.tsx
+++ super-market-ts/src/App.tsx
@@ -31,12 +31,27 @@ function SuperHeader(props: ISuperHeaderProps): any | undefined {
   return <div> <h1 style={{ color: "green" }}> {currentHeader} {dateTime} </h1> </div>
 }
 
+interface IImageProps {
+  imageUrl?: string,
+  height?: number,
+  width?: number
+}
+
+function ImageComponent(props: IImageProps) {
+  const { imageUrl, height, width } = props;
+  const defaultImage: string = "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg"
+  const image = imageUrl || defaultImage
+  return <img src={image} height={height || 150} width={width || 150} />
+}
+
 function App() {
   return (
     <div className="App">
       {/* <MainSuperHeader /> */}
       <SuperHeader headerText='Mini Super' />
       <SuperHeader headerText='Products list' />
+      <ImageComponent imageUrl='https://i.pinimg.com/236x/ca/6d/0f/ca6d0f9248915942ebe0d63038756fad.jpg' height={400} width={500} />
+      <ImageComponent />
       <Products />
     </div >
   );
