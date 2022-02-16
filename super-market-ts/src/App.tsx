7469fb236fa1c20f1cb437b89c8b14c3ff00e8a5
galamo <galamouyal88@gmail.com>
2022-02-16 18:17:24 +0200

    b1

--- super-market-ts/src/App.tsx
+++ super-market-ts/src/App.tsx
@@ -5,12 +5,58 @@ import { JsxElement } from 'typescript';
 
 const dateTime = new Date().toLocaleDateString()
 const headerText = <div> <h1> Mini Super {dateTime} </h1> </div>
-const arrayOfProducts: Array<any> = [<h1> Milk </h1>, <h1> Potatos </h1>]
+// const arrayOfProducts: Array<any> = [<h1> Milk </h1>, <h1> Potatos </h1>]
+const products = [{
+  "name": "hero Product",
+  "detail": "Lorem ipsum dolor sit amet",
+  "price": "100",
+  "hero": "OMG This just came out today!",
+  "image": "http://placehold.it/940x300/999/CCC"
+}, {
+  "name": "Milk",
+  "detail": "Drinks",
+  "price": "5",
+  "info": "This is the latest and greatest product from Derp corp.",
+  "image": "http://placehold.it/300x300/999/CCC"
+}, {
+  "name": "Product 2",
+  "detail": "Lorem ipsum dolor sit amet",
+  "price": "99",
+  "offer": "BOGOF",
+  "image": ""
+}, {
+  "name": "Product 3",
+  "detail": "Lorem ipsum dolor sit amet",
+  "price": "99",
+  "image": "http://placehold.it/300x300/999/CCC"
+}, {
+  "name": "Product 4",
+  "detail": "Lorem ipsum dolor sit amet",
+  "price": "99",
+  "offer": "No srsly GTFO",
+  "image": "http://placehold.it/300x300/999/CCC"
+}, {
+  "name": "Product 5",
+  "detail": "Lorem ipsum dolor sit amet",
+  "price": "99",
+  "image": "http://placehold.it/300x300/999/CCC"
+}, {
+  "name": "Product 6",
+  "detail": "Lorem ipsum dolor sit amet",
+  "price": "99",
+  "info": "This is the latest and greatest product from Derp corp.",
+  "offer": "info with offer",
+  "image": "http://placehold.it/300x300/999/CCC"
+}]
 
 function Products() {
   return <div>
-    {arrayOfProducts.map((header: ReactElement) => {
-      return <div style={{ color: "red" }}>{header} {Math.ceil(Math.random() * 10)}$</div>
+    {products.map((product) => {
+      const { image, name, detail, price } = product
+      return <Product name={name}
+        price={parseInt(price)}
+        imageUrl={image}
+        category={detail} />
     })}
   </div>
 }
@@ -43,6 +89,25 @@ function ImageComponent(props: IImageProps) {
   const image = imageUrl || defaultImage
   return <img src={image} height={height || 150} width={width || 150} />
 }
+// interface IProductProps{}
+function Product(props: {
+  name: string, price: number,
+  imageUrl: string, category: string
+}) {
+  const { name, price, category, imageUrl } = props;
+  return (
+    <div style={{
+      border: "solid 2px black",
+      display: "inline-block", width: "400px"
+    }}>
+      <h1> {name} </h1>
+      <h2> $ {price}  </h2>
+      <h2> {category}  </h2>
+      <ImageComponent imageUrl={imageUrl} />
+    </div>
+  )
+
+}
 
 function App() {
   return (
@@ -50,11 +115,16 @@ function App() {
       {/* <MainSuperHeader /> */}
       <SuperHeader headerText='Mini Super' />
       <SuperHeader headerText='Products list' />
+      <h2> Hi i am the manager: </h2>
       <ImageComponent imageUrl='https://i.pinimg.com/236x/ca/6d/0f/ca6d0f9248915942ebe0d63038756fad.jpg' height={400} width={500} />
-      <ImageComponent />
       <Products />
     </div >
   );
 }
 
 export default App;
+
+
+
+// class User implements IUser { }
+// interface IUser { }
