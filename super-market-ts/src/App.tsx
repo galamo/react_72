b04d67dfe53d5bcdb179870da5d75c9b9ac7dca9
galamo <galamouyal88@gmail.com>
2022-02-16 21:44:12 +0200

    final-lesson-2

--- super-market-ts/src/App.tsx
+++ super-market-ts/src/App.tsx
@@ -1,65 +1,105 @@
 import React, { Component, ReactElement, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FcGrid } from "react-icons/fc"
+import { CgCardSpades } from "react-icons/cg"
+import { AiFillDelete } from "react-icons/ai"
+
 
 
 const dateTime = new Date().toLocaleDateString()
 const headerText = <div> <h1> Mini Super {dateTime} </h1> </div>
 // const arrayOfProducts: Array<any> = [<h1> Milk </h1>, <h1> Potatos </h1>]
-const products = [{
+const data = [{
+  "id": "123324",
   "name": "hero Product",
-  "detail": "Lorem ipsum dolor sit amet",
-  "price": "100",
+  "detail": "Drinks",
+  "price": "3000",
   "hero": "OMG This just came out today!",
   "image": "http://placehold.it/940x300/999/CCC"
 }, {
+  "id": "123333324",
   "name": "Milk",
   "detail": "Drinks",
   "price": "5",
   "info": "This is the latest and greatest product from Derp corp.",
   "image": "http://placehold.it/300x300/999/CCC"
 }, {
+  "id": "12332466767",
   "name": "Product 2",
-  "detail": "Lorem ipsum dolor sit amet",
+  "detail": "Drinks",
   "price": "99",
   "offer": "BOGOF",
   "image": ""
 }, {
+  "id": "19999",
   "name": "Product 3",
-  "detail": "Lorem ipsum dolor sit amet",
+  "detail": "v",
   "price": "99",
   "image": "http://placehold.it/300x300/999/CCC"
 }, {
+  "id": "19996656",
   "name": "Product 4",
-  "detail": "Lorem ipsum dolor sit amet",
+  "detail": "s",
   "price": "99",
   "offer": "No srsly GTFO",
   "image": "http://placehold.it/300x300/999/CCC"
 }, {
+  "id": "199955555",
   "name": "Product 5",
-  "detail": "Lorem ipsum dolor sit amet",
+  "detail": "Drinks",
   "price": "99",
   "image": "http://placehold.it/300x300/999/CCC"
 }, {
+  "id": "12221111",
   "name": "Product 6",
-  "detail": "Lorem ipsum dolor sit amet",
+  "detail": "Drinks",
   "price": "99",
   "info": "This is the latest and greatest product from Derp corp.",
   "offer": "info with offer",
   "image": "http://placehold.it/300x300/999/CCC"
 }]
 
-function Products() {
-  return <div>
-    {products.map((product) => {
-      const { image, name, detail, price } = product
-      return <Product name={name}
+function ProductsPage() {
+  const initialState: boolean = false
+  const [isTableView, setIsTableView] = useState(initialState)
+  const [productsGlobal, setProductsGlobal] = useState(data)
+
+  function _deleteCard(id: string) {
+    if (!id) return;
+    const newProducts = productsGlobal.filter(p => p.id !== id);
+    setProductsGlobal([...newProducts])
+  }
+
+  return (
+    <div key={111213} className='container'>
+      <div style={{ textAlign: "left" }}>
+        <button className='btn btn-success' onClick={() => { setIsTableView(true) }}><FcGrid /> </button>
+        <button className='btn btn-success' onClick={() => { setIsTableView(false) }}><CgCardSpades /> </button>
+      </div>
+      {isTableView ? <ProductsTable products={productsGlobal} /> : <ProductsCards onDeleteFn={_deleteCard} products={productsGlobal} />}
+    </div>
+  )
+}
+
+function ProductsTable(props: { products: Array<any> }) {
+  return <img src="https://www.patternfly.org/v3/pattern-library/content-views/table-view/img/table-overview.png" alt="" />
+
+}
+function ProductsCards(props: { products: Array<any>, onDeleteFn: Function }) {
+  if (!Array.isArray(props.products)) return null;
+  return (<div>
+    {props.products.map((product) => {
+      const { image, name, detail, price, id } = product
+      return <Product onDeleteFn={(id: string) => { props.onDeleteFn(id) }} key={id} id={id} name={name}
         price={parseInt(price)}
         imageUrl={image}
         category={detail} />
     })}
-  </div>
+  </div>)
+
 }
+
 
 function MainSuperHeader() {
   const dateTime = new Date().toLocaleDateString()
@@ -109,31 +149,45 @@ interface IImageProps {
 }
 
 function ImageComponent(props: IImageProps) {
-
   const { imageUrl, height, width } = props;
-  const defaultImage: string = "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg"
+  const defaultImage: string = "https://helios-i.mashable.com/imagery/articles/01eIvreZ9sXEnn1jUU6nyQM/images-93.fit_lim.size_2000x.v1628282411.jpg"
   const image = imageUrl || defaultImage
   return <img src={image} height={height || 150} width={width || 150} />
 }
 // interface IProductProps{}
 function Product(props: {
   name: string, price: number,
-  imageUrl: string, category: string
+  imageUrl: string, category: string, id: string, onDeleteFn: Function
 }) {
-  const { name, price, category, imageUrl } = props;
+  const { name, price, category, imageUrl, id } = props;
+
   return (
     <div style={{
       border: "solid 2px black",
-      display: "inline-block", width: "400px"
+      display: "inline-block", width: "400px", height: "400px"
     }}>
       <h1> {name} </h1>
-      <h2> $ {price}  </h2>
+      <HightPriceInPrivate price={price} />
       <h2> {category}  </h2>
       <ImageComponent imageUrl={imageUrl} />
+      <div>
+        <button className='btn btn-danger' onClick={() => {
+          props.onDeleteFn(id)
+        }}> <AiFillDelete /> </button>
+      </div>
     </div>
   )
 
+  function HightPriceInPrivate(props: { price: number }) {
+    const showPrice = props.price < 1000;
+    return (
+      <>
+        {showPrice ? <h2> $ {price}  </h2 > : <h2> In private </h2>}
+      </>
+    )
+  }
 }
+
 const DEFAULT_UNIT: number = 2
 function App() {
   const initialUnit: number = DEFAULT_UNIT
@@ -150,8 +204,8 @@ function App() {
       <SuperHeader headerText='Mini Super' textSizeUnitChange={unit} />
       <SuperHeader headerText='Products list' textSizeUnitChange={unit} />
       <h2> Hi i am the manager: </h2>
-      <ImageComponent imageUrl='https://i.pinimg.com/236x/ca/6d/0f/ca6d0f9248915942ebe0d63038756fad.jpg' height={400} width={500} />
-      <Products />
+      <ImageComponent imageUrl='https://helios-i.mashable.com/imagery/articles/01eIvreZ9sXEnn1jUU6nyQM/images-93.fit_lim.size_2000x.v1628282411.jpg' height={400} width={500} />
+      <ProductsPage />
     </div >
   );
 }
