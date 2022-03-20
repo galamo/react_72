ea7a3a2f8a6c6ab08ff79c29b18d41da1c7470be
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-03-20 20:39:42 +0200

    pako

--- pako/src/components/pages/appModal/index.tsx
+++ pako/src/components/pages/appModal/index.tsx
@@ -1,18 +1,19 @@
 
 import { Button, Modal } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 export default function AppModal() {
     const appModalErrorState = useSelector((state: any) => state.appModalError)
-    console.log(appModalErrorState)
+    const dispatch = useDispatch();
+
     return (<Modal show={appModalErrorState.isOpen} onHide={() => { }}>
         <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-            <Button variant="secondary" onClick={() => { }}>
+            <Button variant="secondary" onClick={() => { dispatch() }}>
                 Close
             </Button>
         </Modal.Footer>
